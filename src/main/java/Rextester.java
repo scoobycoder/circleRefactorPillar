438902dc22fd6007b9a777b41965b63f25c1e9a3
scoobycoder <scoobycoder@gmail.com>
2014-08-03 20:09:42 -0400

    initial commit with code to refactor

---
+++ src/main/java/Rextester.java
@@ -0,0 +1,52 @@
+//Title of this code
+//'main' method must be in a class 'Rextester'.
+
+import java.util.*;
+import java.lang.*;
+
+class Rextester
+{  
+    public static void main(String args[])
+    {
+        printCircle();
+        printBigCircle();
+        printBiggerCircle();
+    }
+    
+
+    public static void printCircle()
+    {
+        double radius, diameter, circumference, area;
+        
+        diameter = 1;
+        radius = diameter / 2;
+        area = Math.PI * radius * radius;
+        circumference = Math.PI * diameter;
+        
+        System.out.printf("Area is %f and circumference is %f for circle with radius %f and diameter %f\n", area, circumference, radius, diameter);
+    }
+    
+    public static void printBigCircle()
+    {
+        double radius, diameter, circumference, area;
+        
+        diameter = 2;
+        radius = diameter / 2;
+        area = Math.PI * radius * radius;
+        circumference = Math.PI * diameter;
+        
+        System.out.printf("Area is %f and circumference is %f for circle with radius %f and diameter %f\n", area, circumference, radius, diameter);  
+    }
+    
+    public static void printBiggerCircle()
+    {
+        double radius, diameter, circumference, area;
+        
+        diameter = 4;
+        radius = diameter / 2;
+        area = Math.PI * radius * radius;
+        circumference = Math.PI * diameter;
+        
+        System.out.printf("Area is %f and circumference is %f for circle with radius %f and diameter %f\n", area, circumference, radius, diameter);   
+    }
+}
